a77b6014adf523d2701439944dd45c628594ba60
Gabriel Fernando Seffrin <gabrielfseffrin@gmail.com>
2025-04-27 19:44:41 -0300

    configuração do projeto

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SharedModule } from './shared/shared.module'; // o que faz o SharedModule?
+import { CoreModule } from './core/core.module';
 
 @Module({
-  imports: [],
+  imports: [SharedModule, CoreModule],
   controllers: [AppController],
   providers: [AppService],
 })
