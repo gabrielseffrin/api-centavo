d51c539a773a6c91c636da2f1c27e79631ab330b
Gabriel Fernando Seffrin <gabrielfseffrin@gmail.com>
2025-06-08 11:52:58 -0300

    add prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core/core.module';
 import { ExpenseModule } from './expense/expense.module';
 import { ErrorsModule } from './errors/errors.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [SharedModule, CoreModule, ExpenseModule, ErrorsModule],
+  imports: [SharedModule, CoreModule, ExpenseModule, ErrorsModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
